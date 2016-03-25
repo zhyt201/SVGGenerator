7be271f854eeef22baf26ace4bb7ff2e5a2d927d
Yuntao Zhou <Yuntao.Zhou@morningstar.com>
2016-03-25 17:38:45 +0800

    improve gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,52 @@ var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 var fs = require('fs');
 var processTpl = require('./gulp/helpers/processSnippet.js');
+var mainBowerFiles = require('main-bower-files');
+var _ = require('lodash');
+
+function getMainBowerFiles(opts, appendMaps) {
+	var self = this;
+	opts = _.assignIn({
+			includeSelf : false,
+			includeDev : false,
+			debugging : false,
+			checkExistence : true,
+			paths : {
+				bowerDirectory : 'bower_components',
+				bowerrc : '.bowerrc',
+				bowerJson : 'bower.json'
+			}
+		}, opts);
+
+	var mainFiles = mainBowerFiles(opts);
+	var results = [];
+	_.each(mainFiles, function (filePath) {
+		results.push(filePath);
+		if (appendMaps && fs.existsSync(filePath + '.map')) {
+			results.push(filePath + '.map');
+		}
+	});
+
+	return results;
+};
+
+function filterFileType(files, extension) {
+	var regExp = new RegExp('\\.' + extension + '$');
+	return files.filter(regExp.test.bind(regExp));
+};
+
+function inject() {
+	return gulp.src('src/index.html').
+	pipe(plugins.inject(gulp.src(filterFileType(getMainBowerFiles({
+						includeDev : true
+					}), 'js'), {
+				read : false
+			}), {
+			starttag : '<!-- bower:js -->',
+			endtag : '<!-- endbower -->'
+		}))
+	.pipe(gulp.dest('.temp/'));
+};
 
 gulp.task('clean', function () {
 	return gulp.src('build').pipe(plugins.clean({
@@ -24,18 +70,6 @@ gulp.task('preview', function () {
 		fs.writeFileSync('src/index.html', html);
 	}
 	gulp.src('src/index.html')
-	.pipe(plugins.inject(gulp.src('./bower.json')
-			.pipe(plugins.mainBowerFiles({
-					heckExistence : true,
-					paths : {
-						bowerDirectory : 'bower_components',
-						bowerrc : './bowerrc',
-						bowerJson : './bower.json'
-					}
-				})), {
-			starttag : '<!-- bower:js -->',
-			endtag : '<!-- endbower -->'
-		}))
-	.pipe(gulp.dest('.temp/'));
-	gulp.src('./bower.json')
+
+	inject();
 });
